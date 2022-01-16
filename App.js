1745109dc9ea091dbe74d76dec11d7d73a86b15f
eddymandran <eddy.mandran@gmail.com>
2022-01-16 14:57:24 +0100

    adding some style to the input

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View>
-        
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent:'space-between', alignItems:'center'}}>
+        <TextInput
+          placeholder="Course Goal"
+          style={{ width:'80%', borderBottomColor: "black", borderWidth: 1, padding: 10 }}
+        />
+        <Button title="ADD" />
       </View>
-      <View>
-
-      </View>
-      
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
