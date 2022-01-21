fd994095eabf0e8bb94c3d08a717caa06c44fc58
eddymandran <eddy.mandran@gmail.com>
2022-01-21 18:38:44 +0100

    switch from inline style to object style

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {Button, StyleSheet, TextInput, View} from "react-native";
 
 export default function App() {
-  return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent:'space-between', alignItems:'center'}}>
-        <TextInput
-          placeholder="Course Goal"
-          style={{ width:'80%', borderBottomColor: "black", borderWidth: 1, padding: 10 }}
-        />
-        <Button title="ADD" />
-      </View>
-      <View></View>
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Course Goal"
+                    style={styles.input}
+                />
+                <Button title="ADD"/>
+            </View>
+            <View></View>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        borderBottomColor: "black",
+        borderWidth: 1,
+        padding: 10
+    }
+});
