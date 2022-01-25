8a39633c365ad717e5c74f2aed0bf965fbaa0945
eddymandran <eddy.mandran@gmail.com>
2022-01-25 12:15:48 +0100

    Deleting Items

--- App.js
+++ App.js
@@ -10,17 +10,28 @@ export default function App() {
     const addGoalHandler = goalTitle => {
         setCourseGoals(currentGoals => [
             ...currentGoals,
-            { id: Math.random().toString(), value: goalTitle }
+            {id: Math.random().toString(), value: goalTitle}
         ]);
     };
 
+    const removeGoalHandler = goalId => {
+        setCourseGoals(currentGoals => {
+            return currentGoals.filter((goal) => goal.id !== goalId)
+        })
+    }
+
     return (
         <View style={styles.screen}>
-            <GoalInput onAddGoal={addGoalHandler} />
+            <GoalInput onAddGoal={addGoalHandler}/>
             <FlatList
                 keyExtractor={(item, index) => item.id}
                 data={courseGoals}
-                renderItem={itemData => <GoalItem onDelete={()=> console.log('Does that work ?')} title={itemData.item.value} />}
+                renderItem={itemData =>
+                    <GoalItem
+                        id={itemData.item.id}
+                        onDelete={removeGoalHandler}
+                        title={itemData.item.value}
+                    />}
             />
         </View>
     );
